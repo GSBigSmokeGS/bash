9261737a6251c6992ed10c06ca424811b330648e
GSBigSmokeGS <153632788+GSBigSmokeGS@users.noreply.github.com>
2024-04-29 15:16:27 +0000

    sus

--- EsercizioC.sh
+++ EsercizioC.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+# URL del file dei comandi su GitHub
+remote_file_url="https://raw.githubusercontent.com/GSBigSmokeGS/bash/main/comandi.txt"
 
-# Percorso del file dei comandi scaricato
-remote_file_path="comandi.txt"
+# Percorso locale dove salvare il file dei comandi
+local_file_path="comandi.txt"
+
+# Scarica il file dei comandi
+curl -s -o "$local_file_path" "$remote_file_url"
 
 # Loop infinito
 while true
 do
     # Estrai ultimo comando dal file
-    last_command=$(tail -1 "$remote_file_path")
+    last_command=$(tail -1 "$local_file_path")
     
     # Esegue il comando
     eval "$last_command"
